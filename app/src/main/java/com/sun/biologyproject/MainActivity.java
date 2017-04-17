b2613e0d97b1af3c473f683e4b7fd294a62a9ce8
Tony <tony@gmail.com>
2017-04-17 23:46:55 +0800

    liangyuyi

--- app/src/main/java/com/sun/biologyproject/MainActivity.java
+++ app/src/main/java/com/sun/biologyproject/MainActivity.java
@@ -2,12 +2,18 @@ package com.sun.biologyproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button collect;
+    private Button identify;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        collect=(Button)findViewById(R.id.collect_button);
+        identify=(Button)findViewById(R.id.identify_button);
     }
 }
