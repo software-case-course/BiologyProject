25f75b2ac6f9f64354350574e7e98c27c4a88ae5
liangyuyi <1241913849@qq.com>
2017-05-17 13:20:14 +0800

    完成注册登录功能，用Bmob实现数据存储

--- app/src/main/java/com/sun/biologyproject/activity/MainActivity.java
+++ app/src/main/java/com/sun/biologyproject/activity/MainActivity.java
@@ -1,33 +1,60 @@
 package com.sun.biologyproject.activity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationListener;
 import com.sun.biologyproject.R;
+import com.sun.biologyproject.utils.Utils;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Locale;
 
 import cn.bmob.v3.Bmob;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AMapLocationListener{
 
     private Button collect;
     private Button identify;
     private TextView title_text;
     private Button btn_setting;
 
+    //与定位有关的参数
+    private AMapLocationClient locationClient = null;
+    private AMapLocationClientOption locationOption = null;
+    private String[] strMsg;
+    private static String locationString=null;
+    private TextView localText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
+
+        Location();//定位
     }
 
     private void initView(){
         title_text=(TextView)findViewById(R.id.titleText);
         title_text.setText("水生生物识别");
+
+        localText=(TextView)findViewById(R.id.locationText);
 
         collect=(Button)findViewById(R.id.collect_button);
         identify=(Button)findViewById(R.id.identify_button);
@@ -56,5 +83,60 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+
+    @Override
+    public void onLocationChanged(AMapLocation loc) {
+        if (null != loc) {
+            Message msg = mHandler.obtainMessage();
+            msg.obj = loc;
+            msg.what = Utils.MSG_LOCATION_FINISH;
+            mHandler.sendMessage(msg);
+        }
+
+    }
+
+    Handler mHandler = new Handler() {
+        public void dispatchMessage(android.os.Message msg) {
+            switch (msg.what) {
+                //定位完成
+                case Utils.MSG_LOCATION_FINISH:
+                    String result = "";
+                    try {
+                        AMapLocation loc = (AMapLocation) msg.obj;
+                        result = Utils.getLocationStr(loc, 1);
+                        strMsg = result.split(",");
+                        //Toast.makeText(MainActivity.this, "定位成功", Toast.LENGTH_LONG).show();
+                        //textView.setText("地址：" + strMsg[0] + "\n" + "经    度：" + strMsg[1] + "\n" + "纬    度：" + strMsg[1]);
+                        locationString=strMsg[0];
+                        localText.setText("地址："+locationString);
+                    } catch (Exception e) {
+                        Toast.makeText(MainActivity.this, "定位失败", Toast.LENGTH_LONG).show();
+                    }
+                    break;
+                default:
+                    break;
+            }
+        };
+
+    };
+    public void Location() {
+        // TODO Auto-generated method stub
+        try {
+            locationClient = new AMapLocationClient(this);
+            locationOption = new AMapLocationClientOption();
+            // 设置定位模式为低功耗模式
+            locationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Battery_Saving);
+            // 设置定位监听
+            locationClient.setLocationListener(this);
+            locationOption.setOnceLocation(true);//设置为单次定位
+            locationClient.setLocationOption(locationOption);// 设置定位参数
+            // 启动定位
+            locationClient.startLocation();
+            mHandler.sendEmptyMessage(Utils.MSG_LOCATION_START);
+        } catch (Exception e) {
+            Toast.makeText(MainActivity.this, "定位失败", Toast.LENGTH_LONG).show();
+        }
     }
 }
