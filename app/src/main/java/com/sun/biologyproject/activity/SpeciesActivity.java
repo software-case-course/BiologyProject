04b5efddfd23407da2d3386f13fdf18977c14759
Zekooo13 <494851826@qq.com>
2017-05-25 01:18:50 +0800

    工具界面

--- app/src/main/java/com/sun/biologyproject/activity/SpeciesActivity.java
+++ app/src/main/java/com/sun/biologyproject/activity/SpeciesActivity.java
@@ -4,17 +4,26 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.GridView;
 
 import com.sun.biologyproject.R;
+import com.sun.biologyproject.adapter.GridAdapter;
 
 public class SpeciesActivity extends AppCompatActivity {
+
+
+
     @Override
 protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_species);
+
         initView();
+
+
     }
     private void initView()
     {
--- app/src/main/java/com/sun/biologyproject/activity/ToolsActivity.java
+++ app/src/main/java/com/sun/biologyproject/activity/ToolsActivity.java
@@ -1,15 +1,55 @@
 package com.sun.biologyproject.activity;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.TextView;
 
 import com.sun.biologyproject.R;
 
-public class ToolsActivity extends AppCompatActivity {
+public class ToolsActivity extends Activity implements CompoundButton.OnCheckedChangeListener {
+
+    private CheckBox net , bucket , cup , box ,gloves ;
+    private TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tools);
+
+        tv = (TextView)this.findViewById(R.id.tv);
+        net= (CheckBox) this.findViewById(R.id.net);
+        bucket= (CheckBox) this.findViewById(R.id.bucket);
+        cup= (CheckBox) this.findViewById(R.id.cup);
+        box= (CheckBox) this.findViewById(R.id.box);
+        gloves= (CheckBox) this.findViewById(R.id.gloves);
+        net.setOnCheckedChangeListener(this);
+        bucket.setOnCheckedChangeListener(this);
+        cup.setOnCheckedChangeListener(this);
+        box.setOnCheckedChangeListener(this);
+        gloves.setOnCheckedChangeListener(this);
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView , boolean isChecked){
+        String str = "您选择的工具是：";
+        if(net.isChecked()){
+            str+=net.getText()+",";
+        }
+        if(bucket.isChecked()){
+            str+=bucket.getText()+",";
+        }
+        if(cup.isChecked()){
+            str+=cup.getText()+",";
+        }
+        if(box.isChecked()){
+            str+=box.getText()+",";
+        }
+        if(gloves.isChecked()){
+            str+=gloves.getText()+",";
+        }
+        tv.setText(str);
     }
 }
