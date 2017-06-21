ec07dbc93cec277255b4a1475ac88ccd98056a9a
liangyuyi <1241913849@qq.com>
2017-06-21 20:12:04 +0800

    高德地图的地图显示

---
+++ app/src/main/java/com/sun/biologyproject/activity/ShowMapActivity.java
@@ -0,0 +1,169 @@
+package com.sun.biologyproject.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationListener;
+import com.amap.api.maps2d.AMap;
+import com.amap.api.maps2d.CameraUpdateFactory;
+import com.amap.api.maps2d.LocationSource;
+import com.amap.api.maps2d.MapView;
+import com.amap.api.maps2d.UiSettings;
+import com.amap.api.maps2d.model.BitmapDescriptorFactory;
+import com.amap.api.maps2d.model.LatLng;
+import com.amap.api.maps2d.model.MyLocationStyle;
+import com.sun.biologyproject.R;
+
+/**
+ * Created by liangyuyi on 2017/6/19.
+ */
+public class ShowMapActivity extends AppCompatActivity implements LocationSource, AMapLocationListener{
+    private MapView mMapView = null;
+    private AMap aMap;
+    private UiSettings mUiSettings;
+    private MyLocationStyle myLocationStyle;
+    /**
+     * 定位
+     */
+    private AMapLocationClient mLocationClient;
+    private AMapLocationClientOption mLocationOption;
+    private OnLocationChangedListener mListener;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_map);
+        //获取地图控件引用
+        mMapView = (MapView) findViewById(R.id.show_map);
+        //在activity执行onCreate时执行mMapView.onCreate(savedInstanceState)，创建地图
+        mMapView.onCreate(savedInstanceState);
+        //初始化地图控制器对象
+        aMap = mMapView.getMap();
+        mUiSettings = aMap.getUiSettings();
+        aMap.setLocationSource(this);//通过aMap对象设置定位数据源的监听
+        mUiSettings.setMyLocationButtonEnabled(true); //显示默认的定位按钮
+        aMap.setMyLocationEnabled(true);// 可触发定位并显示当前位置
+
+        mUiSettings.setScaleControlsEnabled(true);
+        mUiSettings.setAllGesturesEnabled(true);
+
+        initMapLocationIcon();
+
+    }
+    private void initMapLocationIcon(){
+        myLocationStyle = new MyLocationStyle();//初始化定位蓝点样式类myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE);//连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动。（1秒1次定位）如果不设置myLocationType，默认也会执行此种模式。
+        myLocationStyle.myLocationIcon(BitmapDescriptorFactory.fromResource(R.drawable.location_80));// 设置小蓝点的图标
+        myLocationStyle.radiusFillColor(0);
+        myLocationStyle.strokeColor(0);
+        myLocationStyle.strokeWidth(0);
+        //myLocationStyle.interval(2000); //设置连续定位模式下的定位间隔，只在连续定位模式下生效，单次定位模式下不会生效。单位为毫秒。
+        aMap.setMyLocationStyle(myLocationStyle);//设置定位蓝点的Style
+        //aMap.getUiSettings().setMyLocationButtonEnabled(true);设置默认定位按钮是否显示，非必需设置。
+        aMap.setMyLocationEnabled(true);// 设置为true表示启动显示定位蓝点，false表示隐藏定位蓝点并不进行定位，默认是false。
+    }
+    /**
+     * 初始化定位，并开始定位
+     */
+    private void initLocation(){
+        //初始化定位
+        mLocationClient = new AMapLocationClient(this);
+        //设置定位回调监听，这里要实现AMapLocationListener接口，AMapLocationListener接口只有onLocationChanged方法可以实现，用于接收异步返回的定位结果，参数是AMapLocation类型。
+        mLocationClient.setLocationListener(this);
+        //初始化定位参数
+        mLocationOption = new AMapLocationClientOption();
+        //设置定位模式为Hight_Accuracy高精度模式，Battery_Saving为低功耗模式，Device_Sensors是仅设备模式
+        mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
+        //设置是否返回地址信息（默认返回地址信息）
+        mLocationOption.setNeedAddress(true);
+        //设置是否只定位一次,默认为false
+        mLocationOption.setOnceLocation(false);
+        //设置是否强制刷新WIFI，默认为强制刷新
+        mLocationOption.setWifiActiveScan(true);
+        //设置是否允许模拟位置,默认为false，不允许模拟位置
+        mLocationOption.setMockEnable(false);
+        //设置定位间隔,单位毫秒,默认为2000ms
+        mLocationOption.setInterval(2000);
+        //给定位客户端对象设置定位参数
+        mLocationClient.setLocationOption(mLocationOption);
+        //启动定位
+        mLocationClient.startLocation();
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //在activity执行onDestroy时执行mMapView.onDestroy()，销毁地图
+        mMapView.onDestroy();
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //在activity执行onResume时执行mMapView.onResume ()，重新绘制加载地图
+        mMapView.onResume();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //在activity执行onPause时执行mMapView.onPause ()，暂停地图的绘制
+        mMapView.onPause();
+    }
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        //在activity执行onSaveInstanceState时执行mMapView.onSaveInstanceState (outState)，保存地图当前的状态
+        mMapView.onSaveInstanceState(outState);
+    }
+
+    /**
+     * 激化定位
+     * @param onLocationChangedListener
+     */
+    @Override
+    public void activate(OnLocationChangedListener onLocationChangedListener) {
+        mListener = onLocationChangedListener;
+        if (mLocationClient == null){
+            initLocation();
+        }
+    }
+    /**
+     * 停止定位
+     */
+    @Override
+    public void deactivate() {
+        mListener = null;
+        if (mLocationClient != null) {
+            mLocationClient.stopLocation();
+            mLocationClient.onDestroy();
+        }
+        mLocationClient = null;
+    }
+    /**
+     * 位置变化的回调
+     * @param aMapLocation
+     */
+    @Override
+    public void onLocationChanged(AMapLocation aMapLocation) {
+        if (aMapLocation != null) {
+            if (aMapLocation.getErrorCode() == 0) {
+                //点击定位按钮 能够将地图的中心移动到定位点
+                mListener.onLocationChanged(aMapLocation);
+                //获取定位信息 //点击定位按钮 能够将地图的中心移动到定位点
+                mListener.onLocationChanged(aMapLocation);
+                //设置缩放级别
+                aMap.moveCamera(CameraUpdateFactory.zoomTo(17));
+                //将地图移动到定位点
+                aMap.moveCamera(CameraUpdateFactory.changeLatLng(new LatLng(aMapLocation.getLatitude(), aMapLocation.getLongitude())));
+                //获取定位信息
+                StringBuffer buffer = new StringBuffer();
+                buffer.append(aMapLocation.getCountry() + ""
+                        + aMapLocation.getProvince() + ""
+                        + aMapLocation.getCity() + ""
+                        + aMapLocation.getProvince() + ""
+                        + aMapLocation.getDistrict() + ""
+                        + aMapLocation.getStreet() + ""
+                        + aMapLocation.getStreetNum());
+            }
+        }
+    }
+}
